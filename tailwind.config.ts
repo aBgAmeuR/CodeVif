ed75bfc1f28e6efede3ca5e21275b282de294c6a
aBgAmeuR <a.josset@yahoo.com>
2023-08-19 21:56:11 +0200

    clear projet + setup tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#444444",
+        secondary: "#dfd7af",
+        tertiary: "#f5f3ed",
+        background: "#1c1c1c",
+      },
+      fontFamily: {
+        Lexend: ["Lexend Deca", "sans-serif"],
+        Roboto: ["Roboto Mono", "sans-serif"],
+      },
+      fontSize: {
+        sm: ["12px", {
+            fontWeight: 400,
+            lineHeight: "16px",
+        }],
+        base: ["16px", {
+            fontWeight: 400,
+            lineHeight: "20px",
+        }],
+        lg: ["24px", {
+            fontWeight: 400,
+            lineHeight: "24px",
+        }],
+        xl: ["32px", {
+            fontWeight: 400,
+            lineHeight: "32px",
+        }],
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
