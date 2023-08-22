0862838fad40c1745e84b5e1624e4fb39ce19433
aBgAmeuR <a.josset@yahoo.com>
2023-08-22 23:51:05 +0200

    handle correct and incorrect letter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,24 @@
 "use client";
-import React, { useState, useEffect, ReactElement } from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Text from "@/components/Text";
+
 const Data = "exports.DeleteUser = async (req, res, next) => {\n\ttry {\n\t\tconst username = req.user.username;\n\t\tif (!username) {\n\t\t\treturn next({ status: 400, message: 'Missing input'});\n\t\t}\n\t\ttry {\n\t\t\tconst user = await User.DeleteUser(username);\n\t\t\tres.status(200).send({ error: false, message: 'User deleted' });\n\t\t} catch (error) {\n\t\t\treturn next({ status: 404, message: 'User not found' });\n\t\t}\n\t} catch (error) {\n\t\tnext({ status: 500, message: 'Internal Server Error' });\n\t}\n}";
 
 export default function Home() {
-  // useEffect(() => {
-  //   document.addEventListener("keydown", detectKeyDown);
-  //   return () => {
-  //     document.removeEventListener("keydown", detectKeyDown);
-  //   };
-  // }, [activeWordIndex]);
-
-  // const detectKeyDown = (e: KeyboardEvent) => {
-  //   if (e.key === " ") {
-  //     setActiveWordIndex((prev) => prev + 1);
-  //     setActiveCharIndex(0);
-  //   }
-  // };
-
-  // const DataToHTML = (data: string) => {
-  //   const res = data.split("\n").map((word: string, wordIndex: number) => {
-  //     if (wordIndex === activeLineIndex) {
-  //       return (
-  //         <line key={wordIndex}>
-  //           {word.split(" ").map((char, charIndex) => {
-  //             if (charIndex === activeCharIndex) {
-  //               return (
-  //                 <div className="word active" key={charIndex}>
-  //                   {char}
-  //                 </div>
-  //               );
-  //             } else {
-  //               return <div className="word" key={charIndex}>{char}</div>;
-  //             }
-  //           })}
-  //         </line>
-  //       );
-  //     } else if (wordIndex < activeWordIndex) {
-  //       setActiveLineIndex((prev) => prev + 1);
-  //       setActiveWordIndex(0);
-  //       setActiveCharIndex(0);
-  //     } else {
-  //       return <line key={wordIndex}>{word.split(" ").map((char, chatIndex) => {
-  //         return <div className="word" key={chatIndex}>{char}</div>;
-  //       })}</line>;
-  //     }
-  //   });
-
-  //   return res;
-  // };
-
   const text = new Text(Data);
-  console.log(text);
+  
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      text.keyDownHandler(e.key);
+    });
+    return () => {
+      document.removeEventListener("keydown", (e) => {
+        text.keyDownHandler(e.key);
+      });
+    };
+  }, []);
   
   return (
     <>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,49 +1,98 @@
 import { ReactNode } from "react";
 
-type Line = {
-  words: Word[];
-  tabulation: number;
-};
-type Word = {
-  letters: string[];
-};
-
 export default class Text {
-  private text: Line[] = [];
+  private text: string[];
+  private lineIndex: number;
+  private wordIndex: number;
+  private letterIndex: number;
 
   constructor(text: string) {
-    this.text = text.split("\n").map((line) => {
-      const lineWithoutTab = line.replace(/\t/g, "");
-      return {
-        words: lineWithoutTab.split(" ").map((word) => ({
-          letters: word.trim().split(""),
-        })),
-        tabulation: line.length - lineWithoutTab.length,
-      } as Line;
-    });
+    this.text = text.split("\n");
+    this.lineIndex = 0;
+    this.wordIndex = 0;
+    this.letterIndex = 0;
+  }
+
+  public keyDownHandler(key: string): void {
+    console.log(key, this.lineIndex, this.wordIndex, this.letterIndex);
+  
+    if (key === "Shift") return;
+    else if (key === "Enter") {
+      if (this.wordIndex < this.text[this.lineIndex].split(" ").length - 1) return;
+      this.lineIndex = Math.min(this.lineIndex + 1, this.text.length - 1);
+      this.wordIndex = 0;
+      this.letterIndex = 0;
+    } else if (key === " ") {
+      this.wordIndex = Math.min(this.wordIndex + 1, this.text[this.lineIndex].split(" ").length - 1);
+      this.letterIndex = 0;
+    } else {
+      const currentLetter = this.getCurrentLetter();
+      if (currentLetter === key) {
+        this.addCorrectClass();
+      } else {
+        this.addIncorrectClass();
+      }
+      
+      if (this.letterIndex === this.text[this.lineIndex].split(" ")[this.wordIndex].length - 1) {
+        this.addLetterToWord(); // Add the letter to the word
+      }
+      
+      this.letterIndex = Math.min(this.letterIndex + 1, this.text[this.lineIndex].split(" ")[this.wordIndex].length - 1);
+    }
+  }
+
+  private getCurrentLetter(): string {
+    return this.text[this.lineIndex].split(" ")[this.wordIndex][this.letterIndex];
+  }
+  
+  private addCorrectClass(): void {
+    console.log("correct");
+    const activeLetter = document.querySelector(`line:nth-child(${this.lineIndex + 1}) .word:nth-child(${this.wordIndex + 1}) .letter:nth-child(${this.letterIndex + 1})`);
+    console.log(activeLetter);
+    if (activeLetter) {
+      activeLetter.classList.add("correct"); // Replace "correct-letter" with your actual CSS class for correct letters
+    }
+  }
+  
+  private addIncorrectClass(): void {
+    console.log("incorrect");
+    const activeLetter = document.querySelector(`line:nth-child(${this.lineIndex + 1}) .word:nth-child(${this.wordIndex + 1}) .letter:nth-child(${this.letterIndex + 1})`);
+    console.log(activeLetter);
+    
+    if (activeLetter) {
+      activeLetter.classList.add("incorrect"); // Replace "incorrect-letter" with your actual CSS class for incorrect letters
+    }
+  }
+  
+  private addLetterToWord(): void {
+    // TODO: Add the letter to the current word (e.g., apply a CSS class)
   }
 
   Render(): ReactNode {
-    return this.text.map((line, lineIndex) => {
+    const re = new RegExp("\t", "g");
+
+    return this.text.map((line, index) => {
       return (
-        <line
-          key={lineIndex}
-          style={{ paddingLeft: line.tabulation * 2 + "em" }}
-        >
-          {line.words.map((word, wordIndex) => {
+        <line key={index}>
+          {line.split(" ").map((word, index) => {
+            const wordPadding = word.match(re)?.length ?? 0;
             return (
-              <div key={wordIndex} className="word">
-                {word.letters.map((letter, letterIndex) => {
+              <div
+                className="word"
+                key={index}
+                style={{ paddingLeft: `${wordPadding * 2}em` }}
+              >
+                {word.split("").map((letter, index) => {
+                  if (letter === "\t") return;
                   return (
-                    <span key={letterIndex} className="letter">
-                      {letter}
-                    </span>
+                    <div className="letter" key={index}>
+                      {letter ?? ""}
+                    </div>
                   );
                 })}
               </div>
             );
           })}
-          {lineIndex !== this.text.length - 1 && <br />}
         </line>
       );
     });
